5275ee01c17530b7a0707bb330f38d32d691be06
Venkat Reddy <kvramreddy@gmail.com>
2017-03-01 12:08:48 -0800

    fixes for ubuntu

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,11 +5,28 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 include_recipe 'apt'
 package 'nginx'
-service 'nginx' do
-  action [:enable, :start]
-end
 
 template '/usr/share/nginx/html/index.html' do
   source 'index.html.erb'
   mode '0644'
+end
+
+directory '/etc/nginx' do
+  owner 'root'
+  group 'root'
+  mode 0755
+end
+
+template '/etc/nginx/nginx.conf' do
+  source 'nginx.conf.erb'
+  # notifies :restart, 'service[nginx]', :immediately
+end
+
+template '/etc/nginx/sites-available/default' do
+  source 'default.conf.erb'
+  notifies :restart, 'service[nginx]', :immediately
+end
+
+service 'nginx' do
+  action [:enable, :start]
 end
